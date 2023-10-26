0e71c5ddb996013af36739b6428f71a62c33808f
Jaime Reyes <126992552+jaimereyess@users.noreply.github.com>
2023-10-26 21:25:57 +0100

    connect to database

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -1,10 +1,29 @@
-import mysql from 'serverless-mysql'
+const mysql = require('mysql2');
+import fs from 'fs';
 
-export const conn = mysql({
-  config: {
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    port: parseInt(process.env.MYSQL_PORT),
-    database: process.env.MYSQL_DATABASE
+const caCert = fs.readFileSync('/cacert.pem');
+const clientCert = fs.readFileSync('/cacert.pem');
+const clientKey = fs.readFileSync('/cacert.pem');
+
+export const conn = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  ssl: {
+    ca: caCert,
+    cert: clientCert,
+    key: clientKey
   }
-})
+});
+
+async function connectToDatabase() {
+  try {
+    await conn.connect();
+    console.log('Connected');
+  } catch (error) {
+    console.error('Error connecting to MySQL database:', error);
+  }
+}
+
+export default connectToDatabase;
