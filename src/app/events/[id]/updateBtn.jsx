44d71b6eb746deff3de994dd8e3cdecda728bd28
Jaime Reyes <126992552+jaimereyess@users.noreply.github.com>
2023-11-05 23:46:14 +0000

    update btn

--- src/app/events/[id]/updateBtn.jsx
+++ src/app/events/[id]/updateBtn.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
+import axios from 'axios'; // Importa axios para realizar la solicitud HTTP
 
-const [submit, setSubmit] = useState({
-    submitted: '',
-})
+function UpdateBtn({ params }) {
+    const [submit, setSubmit] = useState({
+        submitted: '',
+    });
 
-const handleSubmitted = async () => {
-    await axios.put('/api/events/' + params.id, submitted)
-}
+    const handleSubmitted = async () => {
+        // Actualiza el estado local con "Si" cuando el botón es presionado
+        setSubmit({ submitted: 'Si' });
 
-function updateBtn() {
+        // Realiza una solicitud PUT al servidor para actualizar el estado en la tabla
+        await axios.put('/api/events/' + params.id, submit);
+    };
 
     return (
         <button
-            className='bg-blue-500 hover:bg-blue-700 py-2 px-3 rounded text-white w-full'
+            className='bg-blue-500 hover:bg-blue-700 py-2 px-3 rounded text-white w-full mt-2'
             onClick={handleSubmitted}
         >
             Entregar
         </button>
-    )
+    );
 }
 
-export default updateBtn
+export default UpdateBtn;
--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -1,20 +1,10 @@
-import mysql from 'mysql2/promise';
-import fs from 'fs';
+import mysql from 'serverless-mysql'
 
-const caCert = fs.readFileSync('/cacert.pem');
-const clientCert = fs.readFileSync('/cacert.pem');
-const clientKey = fs.readFileSync('/cacert.pem');
-
-const CONFIG = {
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  ssl: {
-    ca: caCert,
-    cert: clientCert,
-    key: clientKey
+export const conn = mysql({
+  config: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    port: parseInt(process.env.MYSQL_PORT),
+    database: process.env.MYSQL_DATABASE
   }
-};
-
-export const conn = mysql.createConnection(CONFIG);
+})
